9d208404b4683a427ee3f747f857f392fa5c1cac
avneesh12kumar <ak9500636@gmail.com>
2024-09-19 12:38:13 +0530

    updation

---
+++ models/Student.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const StudentSchema = new Schema({
+    rollNo: { type: String },
+    name: { type: String},
+    fathername: { type: String },
+    adharCardNo: { type: String },
+    mobileNo: { type: String }
+    
+    
+
+
+})
+
+module.exports = mongoose.model('Student', StudentSchema)
